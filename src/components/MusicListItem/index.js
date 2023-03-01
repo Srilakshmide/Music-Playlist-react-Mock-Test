b0d3989425774c4e7746f83e791cb4638fd11abc
Srilakshmi <srilakshmid46@gmail.com>
2023-03-01 17:24:48 +0000

    updated Code

--- src/components/MusicListItem/index.js
+++ src/components/MusicListItem/index.js
@@ -10,21 +10,27 @@ const MusicListItem = props => {
   }
 
   return (
-    <li className="list-items">
-      <img src={imageUrl} alt="track" className="image" />
-      <div className="title-container">
-        <p className="title">{name}</p>
-        <p className="genre">{genre}</p>
+    <li>
+      <div className="list-items">
+        <div className="image-text">
+          <img src={imageUrl} alt="track" className="image" />
+          <div className="title-container">
+            <p className="title">{name}</p>
+            <p className="genre">{genre}</p>
+          </div>
+        </div>
+        <div className="button-time">
+          <p className="time">{duration}</p>
+          <button
+            className="button"
+            type="button"
+            onClick={onClickDelete}
+            data-testid="delete"
+          >
+            <AiOutlineDelete color="#ffffff" size={20} />
+          </button>
+        </div>
       </div>
-      <p className="time">{duration}</p>
-      <button
-        className="button"
-        type="button"
-        onClick={onClickDelete}
-        data-testid="delete"
-      >
-        <AiOutlineDelete color="#ffffff" size={20} />
-      </button>
     </li>
   )
 }
